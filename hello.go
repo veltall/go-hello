3ba736d3e5e54b05736e6821ee8839059ff9ab36
Vu Dinh <vuniverse143@gmail.com>
2016-08-23 22:37:30 -0700

    testing vim

--- hello.go
+++ hello.go
@@ -5,4 +5,6 @@ import "fmt"
 func main() {
 	fmt.Printf("hello, world\n")
 	fmt.Println("Adding a line from VS Code in Arch", "test error")
+	fmt.Println("Adding a line from vim inside xfce4-terminal")
 }
+
